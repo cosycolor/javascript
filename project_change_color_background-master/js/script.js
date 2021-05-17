67423ff5999d9ccf4123a08c778cb91f7536737c
LAPTOP-FN7HK094\Lenovo <handyoon87@gmail.com>
2021-05-17 15:36:15 +0900

    -주석 추가

--- project_change_color_background-master/js/script.js
+++ project_change_color_background-master/js/script.js
@@ -9,3 +9,11 @@ function changeBackground(){
     
     body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
 }
+
+//queryselector 아이디나 클래스 쓸때는 선택자 #,. 사용하는데
+//위에서는 그냥 button만 써야 되네 ->html요소를 가져오는건 그냥 씀 p, a 와 같이 태그요소들
+//이 외에는 .,#선택자 기호를 씀
+
+//queryselector와 getElementById의 차이점은
+//querySelector는 해당 선택자 뭉탱이중 첫번째 요소 반환
+//getElementyById는 해당 요소 반환
