b35193bf8e84626b5dd0abdb0e67d19bd28baaf8
LAPTOP-FN7HK094\Lenovo <handyoon87@gmail.com>
2021-05-28 13:55:08 +0900

    -todoList
    #innerHTML: element안의 node를 지웠다 덮으씌움, 이벤트가 마지막꺼만 걸려있어 insertAdjacent를 사용

--- todo-list-project-2-main/js/app.js
+++ todo-list-project-2-main/js/app.js
@@ -17,38 +17,65 @@ form.addEventListener('submit', (e) =>{
    getList();
 });
 
+clearButton.addEventListener('click',()=>{
+    toDoList = [];
+    getList();
+})
+
 function getList(){
     itemList.innerHTML = '';
 
     toDoList.forEach((list) => {
         let newDiv = '<div class="item my-3">';
-     newDiv += '<h5 class="item-name text-capitalize">'+list+'</h5>';
-     newDiv += '<div class="item-icons">';
-     newDiv += '<a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a>';
-     newDiv += '<a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>';
-     newDiv += '<a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>';
-     newDiv += '</div></div>';
-    
-     //innerText와 innerHtml 차이
-     itemList.innerHTML += newDiv;
+      newDiv += '<h5 class="item-name text-capitalize">'+list+'</h5>';
+      newDiv += '<div class="item-icons">';
+      newDiv += '<a href="#" class="complete-item mx-2 item-icon"><i class="far fa-check-circle"></i></a>';
+      newDiv += '<a href="#" class="edit-item mx-2 item-icon"><i class="far fa-edit"></i></a>';
+      newDiv += '<a href="#" class="delete-item item-icon"><i class="far fa-times-circle"></i></a>';
+      newDiv += '</div></div>';
+
+        itemList.insertAdjacentHTML('beforeend', newDiv );
+        
      modItem(list);
+
+      
+    
+    //  //innerText와 innerHtml, insertAdjacent    
     });
+}
 
 function modItem(list){
     const items = itemList.querySelectorAll('.item');//배열로만들어
 
     items.forEach( (item) =>{
         if(item.querySelector('.item-name').textContent == list){
+            
             item.querySelector('.complete-item').addEventListener('click', ()=>{
                 item.querySelector('.item-name').classList.toggle('completed');
-                this.classList.toggle('visibility');
-            })
-        }
-    })
+                
+            });
 
+            item.querySelector('.edit-item').addEventListener('click', ()=>{
+                itemInput.value = list;
+                itemList.removeChild(item);
+
+                toDoList = toDoList.filter( (item) =>{
+                    return item != list; 
+                });
+            });
+
+            item.querySelector('.delete-item').addEventListener('click',()=>{
+                itemList.removeChild(item);
+
+                toDoList = toDoList.filter( (item) =>{
+                    return item != list;
+                });
+            });
+        }
+    });
 }
     
-}
+
 
    
     
