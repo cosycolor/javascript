cded1cf5d4aa7213f6def40ae4ab973aeb743a55
LAPTOP-FN7HK094\Lenovo <handyoon87@gmail.com>
2021-06-03 19:26:17 +0900

    -grocery-list 2

--- grocery-list-project-2-main/app.js
+++ grocery-list-project-2-main/app.js
@@ -1,139 +1,113 @@
-//Selection items from the DOM
-//Add items container
-const addItemsAction = document.querySelector('.addItems-action');
-const input = document.querySelector('.addItems-input');
-const submit = document.querySelector('.addItems-submit');
+(function(){
+    const addItemAction = document.querySelector('.addItems-action');
+    const inputValue = document.querySelector('.addItems-input');
+    const submit = document.querySelector('.addItems-submit');
+    const groceryList = document.querySelector('.grocery-list');
+    const clearAll = document.querySelector('.displayItems-clear');
 
-//Display items container
-const list = document.querySelector('.grocery-list');
-const displayItemsAction = document.querySelector('.displayItems-action');
-const clear = document.querySelector('.displayItems-clear');
+   
+    document.addEventListener('DOMContentLoaded',getList);
+    //submit으로 하면 왜 새로고침되고 click 으로 해야하는거지?
+    submit.addEventListener('click',addList);
+    groceryList.addEventListener('click',removeItem);
+    clearAll.addEventListener('click',clearAllItems);
+   
+   // clearAll.addEventListener('submit',);
 
-//Add event listeners
-//Submit listener
-submit.addEventListener('click', addItem);
-//Check for local storage
-document.addEventListener('DOMContentLoaded', displayStorage);
-//Clear list
-clear.addEventListener('click', removeItems);
-//Listen to list to delete individual items
-list.addEventListener('click', removeSingleItem);
+    function getList(){
+        let local = localStorage.getItem('groceryList');
 
-
-//functions
-function addItem(event){
-    event.preventDefault();
-    let value = input.value;
-    if (value === ''){
-        showAction(addItemsAction, 'Please add grocery item', false);
-    } else {
-        showAction(addItemsAction, `${value} added to the list`, true);
-        createItem(value);
-        updateStorage(value);
+        if(local){
+            let list = JSON.parse(local);
+            list.forEach((e) => {
+                createDiv(e);
+            });
+        }
     }
-}
 
-function showAction(element, text, value){
-    if (value === true){
-        element.classList.add('success');
-        element.innerText = text;
-        input.value = '';
-        setTimeout(function(){
-            element.classList.remove('success');
-        }, 3000)
-    } else {
-        element.classList.add('alert');
-        element.innerText = text;
-        input.value = '';
-        setTimeout(function(){
-            element.classList.remove('alert');
-        }, 3000)
+    function createDiv(e){
+        let div = document.createElement('div');
+        div.classList.add('grocery-item');
+        div.innerHTML = `<h4 class="grocery-item_title">${e}</h4><a href="#" class="grocery-item_link"><i class="far fa-trash-alt"></i></a>`;
+        groceryList.appendChild(div);
     }
-}
 
-// create item
-function createItem(value){
-    let parent = document.createElement('div');
-        parent.classList.add('grocery-item');
+    function addList(event){
+        event.preventDefault();
+        let input = inputValue.value;
 
-    // let title = document.createElement('h4');
-    //     title.classList.add('grocery-item__title');
-
-    parent.innerHTML = `<h4 class="grocery-item__title">${value}</h4>
-    <a href="#" class="grocery-item__link">
-        <i class="far fa-trash-alt"></i>
-    </a>`
-
-    list.appendChild(parent);
-}
-
-//update storage
-function updateStorage(value){
-    let groceryList;
-    
-    groceryList = localStorage.getItem('groceryList') ? JSON.parse(localStorage.getItem('groceryList')) : [];
-
-    groceryList.push(value);
-    localStorage.setItem('groceryList', JSON.stringify(groceryList));
-}
-
-//display items in local storage
-function displayStorage(){
-    let exists = localStorage.getItem('groceryList');
-    
-    if(exists){
-        let storageItems = JSON.parse(localStorage.getItem('groceryList'));
-        storageItems.forEach(function(element){
-            createItem(element);
-        })
+        if( input == null || input == ''){
+            showText(input,false);
+            
+        }else{
+            showText(input,true);
+            //localStorage에 저장
+            createDiv(input);
+            updateList(input);
+            inputValue.value = '';
+        }
     }
-}
-
-//remove all items
-function removeItems(){
-    //delete from local storage
-    localStorage.removeItem('groceryList');
-    let items = document.querySelectorAll('.grocery-item');
-    
-    if(items.length > 0){
-        //remove each item from the list
-        showAction(displayItemsAction, 'All items deleted', false);
-        items.forEach(function(element){
-            list.removeChild(element);
-        })
-    } else {
-        showAction(displayItemsAction, 'No more items to delete', false);
+    function showText(value, bool){
+        if(bool){
+            addItemAction.classList.add('success');
+            addItemAction.innerHTML = `${value} added to the list`;
+            setTimeout(()=>{
+                addItemAction.classList.remove('success');
+            },3000);
+        }else{
+            addItemAction.classList.add('alert');
+            addItemAction.innerHTML = 'write grocerylist';
+            setTimeout(()=>{
+                addItemAction.classList.remove('alert');
+            },3000);
+        }
     }
-}
 
-//remove single item
-
-function removeSingleItem(event){
-    event.preventDefault();
-    
-    let link = event.target.parentElement;
-    if(link.classList.contains('grocery-item__link')){
-        let text = link.previousElementSibling.innerHTML;
-        let groceryItem = event.target.parentElement.parentElement;
-        //remove from list
-
-        list.removeChild(groceryItem);
-        showAction(displayItemsAction,`${text} removed from the list`, true);
-
-        //remove from local storage
-        editStorage(text);
-
+    function updateList(value){
+        
+        let groceryList = localStorage.getItem('groceryList');
+        
+        if(groceryList){
+            groceryList = JSON.parse(groceryList);
+            groceryList.push(value);
+            localStorage.setItem('groceryList',JSON.stringify(groceryList));
+        }else{
+            groceryList = [];
+            groceryList.push(value);
+            localStorage.setItem('groceryList',JSON.stringify(groceryList));
+        }
     }
-}
+    function removeItem(event){
+        //a링크가 걸려있기때문에 이벤트 취소함
+        event.preventDefault();
+        let link_parent = event.target.parentElement;
+        if(link_parent.classList.contains('grocery-item_link')){
+            let text = link_parent.previousElementSibling.innerHTML;
+            let groceryItem = event.target.parentElement.parentElement;
 
-function editStorage(item){
-    let groceryItems = JSON.parse(localStorage.getItem('groceryList'));
-    let index = groceryItems.indexOf(item);
-    
-    groceryItems.splice(index, 1);
-    //first delete existing list
-    localStorage.removeItem('groceryList');
-    //add new updated/edited list
-    localStorage.setItem('groceryList', JSON.stringify(groceryItems));
+            groceryList.removeChild(groceryItem);
+            editStorage(text);
+        }
+    }
 
-}
+    function editStorage(list){
+        //edit이기 때문에 있는지 없는지 확인할 필요 없이 바로 리스트를 가져온다
+        let groceryList = JSON.parse(localStorage.getItem('groceryList'));
+        let index = groceryList.indexOf(list);
+
+        groceryList.slice(index,1);
+        localStorage.removeItem('groceryList');
+        localStorage.setItem('groceryList',JSON.stringify(groceryList));
+    }
+
+    function clearAllItems(){
+        localStorage.removeItem('groceryList');
+        const items = document.querySelectorAll('.grocery-item');
+            items.forEach((e) => {
+                groceryList.removeChild(e);
+            });
+    }
+
+})();
+
+
