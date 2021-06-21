760347b4aea7af64f11b2a73f4fe8b2518c3fb44
LAPTOP-FN7HK094\Lenovo <handyoon87@gmail.com>
2021-06-21 23:34:50 +0900

    -joke(axios)

--- random-joke-api-project-main/app.js
+++ random-joke-api-project-main/app.js
@@ -1,28 +1,19 @@
-const image = document.querySelector('img');
-const jokeDIV = document.querySelector('#display-joke');
-const button = document.querySelector('#get-joke');
+const image = document.querySelector("img");
+const jokeDIV = document.querySelector("#display-joke");
+const button = document.querySelector("#get-joke");
 
-button.addEventListener('click', function(){
-    getRandomJoke();
-})
+button.addEventListener("click", function () {
+  getRandomJoke();
+});
 
-function getRandomJoke(){
-    const ajax = new XMLHttpRequest;
-    const url = 'https://api.chucknorris.io/jokes/random'
-
-    ajax.open('GET', url, true);
-    ajax.send();
-    ajax.onreadystatechange = function(){
-        if(this.status === 200 && this.readyState === 4){
-            console.log(this.responseText);
-            let data = JSON.parse(this.responseText);
-            jokeDIV.innerHTML = `${data.value}`
-        } else {
-            this.onerror = onerror();
-        }
-    }
-}
-
-function onerror(){
-     jokeDIV.textContent = 'There was an error!';  
+function getRandomJoke() {
+  axios
+    .get("https://api.chucknorris.io/jokes/random")
+    .then((result) => {
+      jokeDIV.innerHTML = `${result.data.value}`;
+    })
+    .catch((error) => {
+      console.log("에러", error);
+      jokeDIV.textContent = "There was an error";
+    });
 }
